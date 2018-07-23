a46d902500130ac5ef9dc4ad8f739e40fc9aea9d
Papabyte <bencreze@yahoo.fr>
2018-07-23 12:06:35 +0200

    delay calendar loading a start

--- start.js
+++ start.js
@@ -20,17 +20,20 @@ const theScore = require('./modules/api_thescore.js');
 
 var assocPeers = [];
 
+setTimeout(loadChampionships,500);
 
-//------The different feeds are added to the calendar
-//------The 2 first arguments specify category and keyword
-mySportFeed.getFixturesAndPushIntoCalendar('Baseball', 'MLB', 'https://api.mysportsfeeds.com/v1.1/pull/mlb/2018-regular/');
-//mySportFeed.getFixturesAndPushIntoCalendar('Basketball', 'NBA', 'https://api.mysportsfeeds.com/v1.1/pull/nba/2018-playoff/');
-//mySportFeed.getFixturesAndPushIntoCalendar('Ice hockey', 'NHL', 'https://api.mysportsfeeds.com/v1.1/pull/nhl/2018-playoff/');
-UfcCom.getFixturesAndPushIntoCalendar('Mixed Martial Arts', 'UFC');
+function loadChampionships(){
+	//------The different feeds are added to the calendar
+	//------The 2 first arguments specify category and keyword
+	mySportFeed.getFixturesAndPushIntoCalendar('Baseball', 'MLB', 'https://api.mysportsfeeds.com/v1.1/pull/mlb/2018-regular/');
+	//mySportFeed.getFixturesAndPushIntoCalendar('Basketball', 'NBA', 'https://api.mysportsfeeds.com/v1.1/pull/nba/2018-playoff/');
+	//mySportFeed.getFixturesAndPushIntoCalendar('Ice hockey', 'NHL', 'https://api.mysportsfeeds.com/v1.1/pull/nhl/2018-playoff/');
+	UfcCom.getFixturesAndPushIntoCalendar('Mixed Martial Arts', 'UFC');
 
-footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','CL', 'https://api.football-data.org/v2/competitions/2001/matches');
-footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','L1', 'https://api.football-data.org/v2/competitions/2015/matches');
-footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','BSA', 'https://api.football-data.org/v2/competitions/2013/matches')
+	footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','CL', 'https://api.football-data.org/v2/competitions/2001/matches');
+	footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','L1', 'https://api.football-data.org/v2/competitions/2015/matches');
+	footballDataOrg.getFixturesAndPushIntoCalendar('Soccer','BSA', 'https://api.football-data.org/v2/competitions/2013/matches')
+}
 
 if (conf.bRunWitness)
 	require('byteball-witness');
